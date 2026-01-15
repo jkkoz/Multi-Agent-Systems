654a447da24fb52056eaec3f730cc123767e2cbf
jkkoz <julka.koziol5@gmail.com>
2026-01-15 17:20:37 +0900

    1. add counters to see if agents can select joint action (c d) or (d c) with higher probability with more iterations
    2. allow for user input to specify the number of games
    
    3. infinite loop so that I can compare results between inputs

--- game.scala
+++ game.scala
@@ -1,34 +1,55 @@
+import scala.util.control.Breaks._
+
 object game {
   def main(args: Array[String]): Unit = {
-    val payoffMatrix1 = Array(
-      Array(1, 5),
-      Array(5, 1)
-    )
+    breakable {
+      while (true) {
+        println("Enter number of games: ")
+        val nGames: Int = scala.io.StdIn.readInt()
+        if (nGames <= 0) break()
 
-    val payoffMatrix2 = Array(
-      Array(1, 5),
-      Array(5, 1)
-    )
+        val payoffMatrix1 = Array(
+          Array(1, 5),
+          Array(5, 1)
+        )
 
-    val agent1 = new Agent(payoffMatrix1)
-    val agent2 = new Agent(payoffMatrix2)
+        val payoffMatrix2 = Array(
+          Array(1, 5),
+          Array(5, 1)
+        )
 
-    for (i <- 1 to 100)
-    {
-      val action1 = agent1.decideNextAction()
-      val action2 = agent2.decideNextAction()
+        var jointCD: Int = 0
+        var jointDC: Int = 0
 
-      agent1.selectedAction = action1
-      agent2.selectedAction = action2
+        val agent1 = new Agent(payoffMatrix1)
+        val agent2 = new Agent(payoffMatrix2)
 
-      val reward1 = agent1.getUtility(action2)
-      val reward2 = agent2.getUtility(action1)
+        for (i <- 1 to nGames) {
+          val action1 = agent1.decideNextAction()
+          val action2 = agent2.decideNextAction()
 
-      agent1.reviseQ(reward1)
-      agent2.reviseQ(reward2)
+          agent1.selectedAction = action1
+          agent2.selectedAction = action2
+
+          (action1, action2) match {
+            case (`c`, `d`) => jointCD += 1
+            case (`d`, `c`) => jointDC += 1
+            case _ => ()
+          }
+
+          val reward1 = agent1.getUtility(action2)
+          val reward2 = agent2.getUtility(action1)
+
+          agent1.reviseQ(reward1)
+          agent2.reviseQ(reward2)
+
+        }
+        println(s"Joint action (C, D) occurred = $jointCD times")
+        println(s"Joint action (D, C) occurred = $jointDC times")
+
+        println(s"Agent1 Q(C) = ${agent1.currentC}, Q(D) = ${agent1.currentD}")
+        println(s"Agent2 Q(C) = ${agent2.currentC}, Q(D) = ${agent2.currentD}")
+      }
     }
-
-    println(s"Agent1 Q(C) = ${agent1.currentC}, Q(D) = ${agent1.currentD}")
-    println(s"Agent2 Q(C) = ${agent2.currentC}, Q(D) = ${agent2.currentD}")
   }
 }
